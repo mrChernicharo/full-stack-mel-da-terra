30dba94b09d478530b186e2940195af349439c7c
Felipe Chernicharo <string7dev@gmail.com>
2022-01-26 23:00:43 -0300

    initializing express server

--- server/server.ts
+++ server/server.ts
@@ -1,3 +1,17 @@
+import express, { Application, Request, Response } from "express";
+
 export function initServer() {
-  console.log("heeey man");
+  const bodyParser = require("body-parser");
+  const app: Application = express();
+
+  app.route("/").get((req, res) => res.status(200).send("<h1>API up and running!</h1>"));
+
+  app.route("/test").get((req: Request, res: Response) => {
+    res.json({ message: `get request works!` });
+  });
+
+  const PORT = process.env.PORT || 9000;
+  app.listen(PORT, () => {
+    console.log(`server running! Listening to port:${PORT}`);
+  });
 }
